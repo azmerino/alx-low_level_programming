700b3fd3e8c8000ec88a9654bf73932a5ea1e54f
azmerino <filimine34@yahoo.com>
2022-12-11 10:42:31 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,5 +5,14 @@
  *   
  * Return: Always 0 (Success)
  */
+int mian(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+        printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n, %i byte(s)\n", sizeof(float));
+	return (0);
+}
 
 
