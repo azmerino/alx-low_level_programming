6c289d864d7adced4fc101086d237fd5954e4249
azmerino <filimine34@yahoo.com>
2022-12-12 02:36:28 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Descritio: Get a random t with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'a';
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
+
